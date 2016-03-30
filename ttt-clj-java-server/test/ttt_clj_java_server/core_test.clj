851c6593dc7188fa46ebe9a1476dc5dd7b5fd01b
dholness2 <DHolness@Dons-MacBook-Pro.local>
2016-03-29 22:04:46 -0700

    first commit

---
+++ ttt-clj-java-server/test/ttt_clj_java_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns ttt-clj-java-server.core-test
+  (:require [clojure.test :refer :all]
+            [ttt-clj-java-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
