43cdf49dbafaccbc630ea770b9212844a201e85e
Donovan Holness <dholness@8thlight.com>
2016-04-08 15:48:53 -0700

    added Server api

--- ttt-clj-java-server/test/ttt_clj_java_server/api/server_test.clj
+++ ttt-clj-java-server/test/ttt_clj_java_server/api/server_test.clj
@@ -6,18 +6,24 @@
   (:import com.javawebserver.app.Server))
 
 (deftest new-server-builder-test
-  (testing "creates Java SimpleServerBuilder instance"
+  (testing "it creates Java SimpleServerBuilder instance"
     (is (instance? SimpleServerBuilder (new-server-builder)))))
 
 (deftest get-server-test
-  (testing "returns Java Server instance"
+  (testing "it returns Java Server instance"
     (let [builder (new-server-builder)]
-      (is (instance? Server (get-server 9000 builder))))))
+      (is (instance? Server (get-server 9001 builder))))))
 
 (deftest add-route-test
-  (testing "adds routes to ServerBuilder instance"
+  (testing "it adds routes to ServerBuilder instance"
     (let [builder (new-server-builder)
           response (game)]
       (add-route "GET /" response builder)
       (is (contains? (get-routes builder) "GET /")))))
 
+(deftest server-off-test
+  (testing "it turns server off"
+    (let [builder (new-server-builder)
+          server (get-server 8989 builder)]
+      (server-off server)
+      (is (false? (serveron? server))))))
