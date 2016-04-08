29e11f46be90d61cf79f1f941a533338a738b543
Donovan Holness <dholness@8thlight.com>
2016-04-08 15:57:07 -0700

    board builder updates

--- ttt-clj-java-server/test/ttt_clj_java_server/board_builder_test.clj
+++ ttt-clj-java-server/test/ttt_clj_java_server/board_builder_test.clj
@@ -30,29 +30,29 @@
   (testing "it creates table data on position marker"
     (let [index 0
           marker "_"
-          expected "<td><input type=\"radio\" value=\"1\" name=\"openPosition\"></td>"]
+          expected "<td class=\"square0\"><input type=\"radio\" value=\"1\" name=\"openPosition\"></td>"]
       (is (= expected (create-table-data index marker))))))
 
 (deftest build-row-data-test
   (testing "builds required table data based on board"
     (let [board [[["x" "_" "_"]]]
-          expected['("<td>x<input type=\"hidden\" value=\"1\" name=\"x\"></td>")
-                    '("<td><input type=\"radio\" value=\"2\" name=\"openPosition\"></td>")
-                    '("<td><input type=\"radio\" value=\"3\" name=\"openPosition\"></td>")]]
+          expected['("<td class=\"square0\"><a class=\"move\">x</a><input type=\"hidden\" value=\"1\" name=\"x\"></td>")
+                    '("<td class=\"square1\"><input type=\"radio\" value=\"2\" name=\"openPosition\"></td>")
+                    '("<td class=\"square2\"><input type=\"radio\" value=\"3\" name=\"openPosition\"></td>")]]
       (is (= expected (build-rows board))))))
 
 (deftest winner-header-test
   (testing "it builds winner header"
    (let [board [["x""x""x"]["o""_""o"]["_""_""_"]]
          winner (winner board)
-         expected-header (str "<h2> Winner: " winner "!</h2>")]
+         expected-header (str "<h2>"winner"!</h2>")]
      (is (= expected-header (winner-header board))))))
 
 (deftest winner-header-test-false-state
   (testing "it builds winner header"
    (let [board [["x""_""x"]["o""_""o"]["_""_""_"]]
          winner (winner board)
-         expected-header "<h2> Select move</h2>"]
+         expected-header "<h2>Select move</h2>"]
      (is (= expected-header (winner-header board))))))
 
 (deftest form-submit-test
@@ -71,28 +71,36 @@
   (testing "it builds board view based on board"
     (let [board [["_""_""_"]["_""_""_"]["_""_""_"]]
           expected(str "<html>"
-                     "<h1>Unbeatable Tic-Tac-Toe</h1>"
-                     "<h2> Select move</h2>"
-                     "<body>"
-                      "<form action=\"/move\" method=\"POST\">"
-                        "<table>"
-                          "<tr>"
-                            "<td><input type=\"radio\" value=\"1\" name=\"openPosition\"></td>"
-                            "<td><input type=\"radio\" value=\"2\" name=\"openPosition\"></td>"
-                            " <td><input type=\"radio\" value=\"3\" name=\"openPosition\"></td>"
-                        "</tr>"
-                        "<tr>"
-                           "<td><input type=\"radio\" value=\"4\" name=\"openPosition\"></td>"
-                           "<td><input type=\"radio\" value=\"5\" name=\"openPosition\"></td>"
-                           "<td><input type=\"radio\" value=\"6\" name=\"openPosition\"></td>"
-                        "</tr>"
-                        "<tr>"
-                           "<td><input type=\"radio\" value=\"7\" name=\"openPosition\"></td>"
-                           "<td><input type=\"radio\" value=\"8\" name=\"openPosition\"></td>"
-                           "<td><input type=\"radio\" value=\"9\" name=\"openPosition\"></td>"
-                        "</tr>"
-                        "</table><input type=\"submit\" value=\"Submit Move\" name=\"move\">"
-                      "</form><p><a href=/>New Game</a></p>"
-                    "</body>"
-                   "</html>")]
+                         "<head>"
+                           "<style>"
+                             (slurp "resources/board-style.css")
+                           "</style>"
+                         "</head>"
+                         "<h1>Unbeatable Tic-Tac-Toe</h1>"
+                         "<body>"
+                         "<form action=\"/move\" method=\"POST\">"
+                          "<table>"
+                            "<tr>"
+                              "<td class=\"square0\"><input type=\"radio\" value=\"1\" name=\"openPosition\"></td>"
+                              "<td class=\"square1\"><input type=\"radio\" value=\"2\" name=\"openPosition\"></td>"
+                              "<td class=\"square2\"><input type=\"radio\" value=\"3\" name=\"openPosition\"></td>"
+                            "</tr>"
+                            "<tr>"
+                              "<td class=\"square3\"><input type=\"radio\" value=\"4\" name=\"openPosition\"></td>"
+                              "<td class=\"square4\"><input type=\"radio\" value=\"5\" name=\"openPosition\"></td>"
+                              "<td class=\"square5\"><input type=\"radio\" value=\"6\" name=\"openPosition\"></td>"
+                            "</tr>"
+                            "<tr>"
+                              "<td class=\"square6\"><input type=\"radio\" value=\"7\" name=\"openPosition\"></td>"
+                              "<td class=\"square7\"><input type=\"radio\" value=\"8\" name=\"openPosition\"></td>"
+                              "<td class=\"square8\"><input type=\"radio\" value=\"9\" name=\"openPosition\"></td>"
+                            "</tr>"
+                          "</table>"
+                          "<input type=\"submit\" value=\"Submit Move\" name=\"move\"><br>"
+                        "</form>"
+                    "<p>"
+                     "<a href=/>New Game</a>"
+                   "</p>"
+                   "</body>"
+                "</html>")]
      (is (= expected (get-board-view board))))))
