2d4bec41b2ee3ba0242d2740c49994753846fb19
Donovan Holness <dholness@8thlight.com>
2016-04-08 15:48:11 -0700

    added core

--- ttt-clj-java-server/test/ttt_clj_java_server/core_test.clj
+++ ttt-clj-java-server/test/ttt_clj_java_server/core_test.clj
@@ -1,7 +1,7 @@
 (ns ttt-clj-java-server.core-test
   (:require [clojure.test :refer :all]
             [ttt-clj-java-server.core :refer :all]
-            [ttt-clj-java-server.api.server :refer [add-route get-server new-server-builder]])
+            [ttt-clj-java-server.api.server :refer [add-route get-routes get-server new-server-builder]])
   (:import com.javawebserver.app.Server))
 
 (deftest build-routes-test
@@ -12,6 +12,6 @@
       (is (expected-routes (get-routes builder))))))
 
 (deftest build-routes-test
-  (testing "it builds app sever"
-    (let [port 9000]
+  (testing "it builds app server"
+    (let [port 9510]
       (is (instance? Server (build-server port))))))
