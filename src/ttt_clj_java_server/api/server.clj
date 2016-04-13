837cea894f9cf3fbcf837a3f64a27079c77a7907
Donovan Holness <dholness@8thlight.com>
2016-04-13 10:41:16 -0700

    updated server api

--- src/ttt_clj_java_server/api/server.clj
+++ src/ttt_clj_java_server/api/server.clj
@@ -1,18 +1,19 @@
 (ns ttt-clj-java-server.api.server
+  (:import com.javawebserver.app.builds.BasicBuild)
   (:import com.javawebserver.app.serverBuilders.SimpleServerBuilder)
   (:import com.javawebserver.app.Server))
 
-(defn new-server-builder []
-  (SimpleServerBuilder.))
+(defn new-server-build []
+  (BasicBuild. (SimpleServerBuilder.)))
 
-(defn add-route [route response server-builder]
-  (.addRoute server-builder route response))
+(defn add-route [route response server-build]
+  (.addRoute server-build route response))
 
-(defn get-routes [server-builder]
-  (.getRoutes server-builder))
+(defn get-routes [server-build]
+  (.getRoutes server-build))
 
-(defn get-server [port server-builder]
-  (.getServer server-builder port))
+(defn get-server [port server-build]
+  (.getServer server-build port))
 
 (defn serveron? [server]
   (.isServerOn server))
--- test/ttt_clj_java_server/api/server_test.clj
+++ test/ttt_clj_java_server/api/server_test.clj
@@ -1,29 +1,30 @@
 (ns ttt-clj-java-server.api.server-test
   (:require [clojure.test :refer :all]
             [ttt-clj-java-server.responses.game-response :refer [game]]
-            [ttt-clj-java-server.api.server :refer [serveron? server-off new-server-builder get-server add-route get-routes]])
+            [ttt-clj-java-server.api.server :refer [serveron? server-off new-server-build get-server add-route get-routes]])
+  (:import com.javawebserver.app.builds.BasicBuild)
   (:import com.javawebserver.app.serverBuilders.SimpleServerBuilder)
   (:import com.javawebserver.app.Server))
 
 (deftest new-server-builder-test
   (testing "it creates Java SimpleServerBuilder instance"
-    (is (instance? SimpleServerBuilder (new-server-builder)))))
+    (is (instance? BasicBuild (new-server-build)))))
 
 (deftest get-server-test
   (testing "it returns Java Server instance"
-    (let [builder (new-server-builder)]
+    (let [builder (new-server-build)]
       (is (instance? Server (get-server 9001 builder))))))
 
 (deftest add-route-test
   (testing "it adds routes to ServerBuilder instance"
-    (let [builder (new-server-builder)
+    (let [builder (new-server-build)
           response (game)]
       (add-route "GET /" response builder)
       (is (contains? (get-routes builder) "GET /")))))
 
 (deftest server-off-test
   (testing "it turns server off"
-    (let [builder (new-server-builder)
+    (let [builder (new-server-build)
           server (get-server 8989 builder)]
       (server-off server)
       (is (false? (serveron? server))))))
