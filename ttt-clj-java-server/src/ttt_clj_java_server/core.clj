238da04deb062f32259565caef362f0fb624e5c4
Donovan Holness <dholness@8thlight.com>
2016-04-13 08:39:06 -0700

    updated core with server functions

--- ttt-clj-java-server/src/ttt_clj_java_server/core.clj
+++ ttt-clj-java-server/src/ttt_clj_java_server/core.clj
@@ -1,7 +1,15 @@
 (ns ttt-clj-java-server.core
-  (:gen-class))
+  (:require [ttt-clj-java-server.responses.game-response :refer [game]]
+            [ttt-clj-java-server.api.server :refer [add-route get-server]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn build-routes[port]
+ (add-route "GET /" (game))
+ (add-route "POST /move" (game)))
+
+(defn run-server [port]
+  (build-routes port)
+  (let [server (get-server port)]
+    (.run server)))
+
+(defn -main [& args]
+  (run-server 8100))
